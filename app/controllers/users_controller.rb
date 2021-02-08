04a2a938b6a4c06d7b7d8ac02e6377f1205152b7
be-chan <70454487+be-chan@users.noreply.github.com>
2021-02-08 23:38:54 +0900

    ユーザー登録、編集、ログイン、ログアウト、退会機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save 
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      redirect_to root_path
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:nickname,:email, :password, :password_confirmation, :full_name, :full_name_kana, :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :job_id, :age_id, :gender_id)
+  end
 end
