97770c87c1faaa5edfe2cee28b6a60a2881baf64
be-chan <70454487+be-chan@users.noreply.github.com>
2021-02-22 18:12:23 +0900

    usercontrollerのリファクタリング

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:show]
+  before_action :user_find_params, only: [:show, :edit, :destroy]
 
   def new
     @user = User.new
@@ -11,15 +12,14 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+
   end
 
   def edit
-    @user = User.find(params[:id])
+    
   end
 
   def destroy
-    @user = User.find(params[:id])
     if @user.destroy
       redirect_to root_path
     end
@@ -28,5 +28,9 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:nickname,:email, :password, :password_confirmation, :full_name, :full_name_kana, :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :job_id, :age_id, :gender_id)
+  end
+
+  def user_find_params
+    @user = User.find(params[:id])
   end
 end
